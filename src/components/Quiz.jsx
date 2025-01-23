a898c7d186b78c75e5fb0f79b719ad61157577fe
RakeshkumarThera <rakeshkumarthera@gmail.com>
2025-01-23 15:23:08 -0500

    added styling for correct, wronf and selected answers but there is bug of answers jumping around that is to be fixed in next commit

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,15 +5,31 @@ import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz(){
     const[userAnswers, setUserAnswers] = useState([]);
+    const[answerState, setAnswerState] = useState('');
 
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length -1;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
+        setAnswerState('answered');
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer]
         });
-    }, []);
+
+        setTimeout(()=>{
+            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+                setAnswerState('correct');
+            } else {
+                setAnswerState('wrong');
+            }
+
+            setTimeout(()=>{
+                setAnswerState('');
+            }, 2000);
+
+        }, 1000);
+
+    }, [activeQuestionIndex]);
 
     const handleSkipAnswer = useCallback(()=> handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -25,7 +41,7 @@ export default function Quiz(){
     }
 
     const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);{}
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return(
 
@@ -38,13 +54,28 @@ export default function Quiz(){
                 />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>
+                    {shuffledAnswers.map((answer) => {
+                        const isSelected = userAnswers[userAnswers.length-1] === answer;
+                        let cssClasses = '';
+
+                        if(answerState === 'answered' && isSelected){
+                            cssClasses = 'selected';
+                        }
+
+                        if(answerState === 'correct' || answerState === 'wrong' && isSelected){
+                            cssClasses = answerState;
+                        }
+
+                        return(
+                            <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)} className={cssClasses}>
                                 {answer}
                             </button>
-                        </li>
-                    ))}
+                            </li>
+                        )
+                       
+                    }
+                    )}
                 </ul>
             </div>
         </div>
